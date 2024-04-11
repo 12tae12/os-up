916e3a33c7295473f5cac2c855ca4275664c4946
12tae12 <84397906+12tae12@users.noreply.github.com>
2024-04-11 11:19:03 -0400

    Ready to upgrade

---
+++ upgrade.sh
@@ -0,0 +1,30 @@
+cd ~
+echo '  _ ____ _____          _ ____  
+ / |___ \_   _|_ _  ___/ |___ \ 
+ | | __/ || |/ _` |/ _ \ | __) |
+ | |/ __/ | | (_| |  __/ |/ __/ 
+ |_|_____||_|\__,_|\___|_|_____|
+                                '
+echo '**12Tae12 Software basic system upgrade Version 1.0.1**'
+echo 'Running basic upgrade...'
+echo 'Ckecking if installer depends are OK.'
+echo 'Please give root/sudo password.'
+su root
+sudo apt install aptitude --assume-yes
+echo 'Updating system...'
+sudo aptitude update
+sudo aptitude upgrade
+echo 'Removing old files...'
+sudo rm -d -r ~/os-up
+
+echo 'Asking github for updates...'
+git clone --recurse-submodules https://github.com/12tae12/os-up
+echo 'Updated script is now installed. Running new script.'
+cd ~/os-up
+sudo chmod +x ./os-up.sh
+gnome-terminal -- ./os-up.sh
+
+echo 'Done.'
+echo 'If you want to run the script again, run the command:'
+echo 'cd ~/os-up'
+echo 'sudo chmod +x ./upgrade.sh'
